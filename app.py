340fb3c2e282fd176777eeae6f971fae90498f5d
Timofey21 <timofey.milov41@gmail.com>
2023-12-13 17:14:22 +0000

    some improvements

--- app.py
+++ app.py
@@ -1,28 +1,28 @@
 import sqlite3
 
+from flask import Flask, render_template, render_template_string, request, redirect, session, abort
+from database import init_db
 
-from flask import Flask, render_template, render_template_string, request, redirect
 
 app = Flask(__name__)
 
+app.secret_key = 'BAD_SECRET_KEY'
 
-def init_db():
-    with sqlite3.connect('demo.db') as db:
-        db.execute('CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, username TEXT, password TEXT)')
 
-        # db.execute("INSERT OR IGNORE INTO users (username, password) VALUES ('admin', 'password123')")
-        # db.execute("INSERT OR IGNORE INTO users (username, password) VALUES ('bob', 'bobpass')")
-        # db.execute("INSERT OR IGNORE INTO users (username, password) VALUES ('alice', 'alicepass')")
+@app.before_request
+def require_login():
+    allowed_routes = ["login", "register"]
+    if request.endpoint not in allowed_routes and 'user' not in session:
+        return redirect('/login')
 
 
 @app.route('/login', methods=['POST', 'GET'])
 def login():  # put application's code here
 
     init_db()
-    message = ""
     if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
+        username = request.form.get('username')
+        password = request.form.get('password')
 
         connection = sqlite3.connect('demo.db')
         cursor = connection.cursor()
@@ -31,22 +31,49 @@ def login():  # put application's code here
         connection.close()
 
         if user:
-            message = "Login successful!"
-            return redirect('/', code=302)
+            if user[-2] == 'admin':
+                session['user'] = user[-2]
+                return redirect('/admin')
+            else:
+                session['user'] = user[-2]
+                return redirect('/')
+
         else:
             message = "Login failed!"
+            return render_template_string(open('templates/login.html').read(), message=message)
+    return render_template('login.html')
 
-    return render_template_string(open('templates/login.html').read(), message=message)
 
-
-@app.route('/register')
+@app.route('/register', methods=['POST', 'GET'])
 def register():
+    if request.method == 'POST':
+
+        connection = sqlite3.connect('demo.db')
+        cursor = connection.cursor()
+
+        username = request.form.get('username')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+
+        cursor.execute("SELECT * FROM users WHERE username = '{}'".format(username))
+        username_check = cursor.fetchone()
+        connection.commit()
+
+
+        if username and password1 == password2 and username_check != username:
+            cursor.execute('INSERT INTO users (username, password) VALUES (?, ?)', (username, password1))
+            connection.commit()
+
+        if username == 'admin':
+            return redirect('/admin')
+
+        return redirect('/login')
+
     return render_template("register.html")
 
 
 @app.route('/', methods=['POST', 'GET'])
 def main_page():
-
     with sqlite3.connect('demo.db') as db:
         db.execute('CREATE TABLE IF NOT EXISTS blogs (id INTEGER PRIMARY KEY, message TEXT)')
         # db.execute("INSERT INTO blogs (message) VALUES ('bla bla')")
@@ -68,11 +95,33 @@ def main_page():
             cursor.execute("DELETE FROM blogs WHERE id = ('{}')".format(delete_id))
             connection.commit()
 
+        logout_button = request.form.get('logout-button')
+
+        if logout_button:
+            session.pop('user', default=None)
+            return redirect('/login')
+
     cursor.execute("SELECT * FROM blogs")
     connection.commit()
     blogs = cursor.fetchall()
     connection.close()
     return render_template("index.html", blogs=blogs)
+
+
+@app.route('/admin', methods=['POST', 'GET'])
+def admin():
+    if session['user'] != 'admin':
+        abort(403)
+
+    if request.method == 'POST':
+        logout_button = request.form.get('logout-button')
+        if logout_button:
+            session.pop('user', default=None)
+            return redirect('/login')
+
+    return render_template("admin.html")
+
+
 
 
 if __name__ == '__main__':
---
+++ database.py
@@ -0,0 +1,10 @@
+import sqlite3
+
+
+def init_db():
+    with sqlite3.connect('demo.db') as db:
+        db.execute('CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, username TEXT, password TEXT)')
+
+        # db.execute("INSERT OR IGNORE INTO users (username, password) VALUES ('admin', 'password123')")
+        # db.execute("INSERT OR IGNORE INTO users (username, password) VALUES ('bob', 'bobpass')")
+        # db.execute("INSERT OR IGNORE INTO users (username, password) VALUES ('alice', 'alicepass')")
